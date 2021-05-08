8c7ca034ae7537a6f52c54f12b2d153bc221bc11
Aad 't Hart <aad@aadjemonkeyrock.com>
2021-05-08 16:34:41 +0200

    Initial Commit

---
+++ SplitView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitView
+//
+//  Created by Aad 't Hart on 08/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
